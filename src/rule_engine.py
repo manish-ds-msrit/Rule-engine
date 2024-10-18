c79ce49724b3c235daa267c2673f2ae97af6eca3
Manish D S <divatemanish6@gmail.com>
2024-10-18 22:43:10 +0530

    Update rule_engine.py

--- src/rule_engine.py
+++ src/rule_engine.py
@@ -1,1 +1,10 @@
+class Node:
+    def __init__(self, type, left=None, right=None, value=None):
+        self.type = type  # 'operator' or 'operand'
+        self.left = left  # left child
+        self.right = right  # right child for operators
+        self.value = value  # value for operand nodes
+
+    def __repr__(self):
+        return f'Node(type={self.type}, left={self.left}, right={self.right}, value={self.value})'
 
